8e06b90bec4c333313323cf6a368c07b8dcb29a2
bsc-com-ne-21-19 <bsc-com-ne-21-19@unima.ac.mw>
2024-03-16 09:18:41 +0200

    Initial Commit

---
+++ src/main/java/com/geofence/geofencing_backend/GeofencingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.geofence.geofencing_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeofencingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeofencingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geofence/geofencing_backend/GeofencingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geofence.geofencing_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeofencingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
