034b05066f05a83d170ec5d8028fd708ab3b4be6
bsc-com-ne-21-19 <bsc-com-ne-21-19@unima.ac.mw>
2024-04-25 09:21:58 +0200

    Modified Fence Entity and API to support Geometry Spatial data

--- src/main/java/com/geofence/geofencing_backend/controllers/FenceController.java
+++ src/main/java/com/geofence/geofencing_backend/controllers/FenceController.java
@@ -4,22 +4,21 @@ package com.geofence.geofencing_backend.controllers;
  * Fence Controller
  * Author: James Kalulu (Bsc-com-ne-21-19)
  * Created on: 27-03-2024
- * Last Modified on: 31-03-2024
+ * Last Modified on: 25-04-2024
  * Last Modified by: James Kalulu (Bsc-com-ne-21-19)
  */
 
 import com.geofence.geofencing_backend.entities.Fence;
 import com.geofence.geofencing_backend.services.FenceService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 
 @RestController
-@RequestMapping(path = "/api/fence")
+@RequestMapping(path = "/api/v1/fence")
 public class FenceController {
 
 
@@ -34,15 +33,37 @@ public class FenceController {
 
     /*
         HTTP POST request handler
+
+        i.e., http://localhost:8080/api/v1/fence
         Receives the following parameters to create a fence instance
         {
-            "name": null,
-            "description": null,
-            "centerLongitude": null,
-            "centerLatitude": null,
-            "centerRadius": null,
-            "logs": null,
-            "active": null
+            "id": 1,
+            "name": "My Fence 1",
+            "description": "A description of my fence 1",
+            "boundary": {
+                "type": "Polygon",
+                "coordinates": [
+                    [
+                        [
+                            10.0,
+                            20.0
+                        ],
+                        [
+                            15.0,
+                            25.0
+                        ],
+                        [
+                            20.0,
+                            30.0
+                        ],
+                        [
+                            10.0,
+                            20.0
+                        ]
+                    ]
+                ]
+            },
+            "is_active": true
         }
      */
     @PostMapping
@@ -69,7 +90,7 @@ public class FenceController {
 
     /*
         HTTP GET request handler
-        Receives no parameters to return a list of fences instances
+        Receives no parameters to return a list of all fences instances
      */
     @GetMapping
     public ResponseEntity<List<Fence>> getAllEventLogs() {
@@ -85,6 +106,39 @@ public class FenceController {
     /*
         HTTP PUT/id request handler
         Receives the id parameters to update a fence instance by id if it exists
+
+        i.e., http://localhost:8080/api/v1/fence/2
+
+        Receives the following parameters to update an existing fence instance
+        {
+            "id": 1,
+            "name": "My Fence 1",
+            "description": "An updated description of my fence 1",
+            "boundary": {
+                "type": "Polygon",
+                "coordinates": [
+                    [
+                        [
+                            10.0,
+                            20.0
+                        ],
+                        [
+                            15.0,
+                            25.0
+                        ],
+                        [
+                            20.0,
+                            30.0
+                        ],
+                        [
+                            10.0,
+                            20.0
+                        ]
+                    ]
+                ]
+            },
+            "is_active": false
+        }
      */
     @PutMapping("/{id}")
     public ResponseEntity<Fence> updateFence(@PathVariable Long id, @RequestBody Fence fence) {
@@ -97,6 +151,8 @@ public class FenceController {
     /*
         HTTP DELETE/id request handler
         Receives the id parameter to delete a fence  instance by id if it exists
+
+        i.e., http://localhost:8080/api/v1/fence/2
      */
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteFence(@PathVariable Long id) {
--- src/main/java/com/geofence/geofencing_backend/entities/Fence.java
+++ src/main/java/com/geofence/geofencing_backend/entities/Fence.java
@@ -4,28 +4,23 @@ package com.geofence.geofencing_backend.entities;
  * Fence entity
  * Author: James Kalulu (Bsc-com-ne-21-19)
  * Created on: 26-03-2024
- * Last Modified on: 26-03-2024
+ * Last Modified on: 25-04-2024
  * Last Modified by: James Kalulu (Bsc-com-ne-21-19)
  */
 
+import com.fasterxml.jackson.annotation.*;
 import jakarta.persistence.*;
+import lombok.Data;
+import org.locationtech.jts.geom.Polygon;
 
 import java.util.List;
 
+@Data
 @Entity
 @Table(name = "Fence")
 public class Fence {
 
     @Id
-    @SequenceGenerator(
-            name = "fence_sequence",
-            sequenceName = "fence_sequence",
-            allocationSize = 1
-    )
-    @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "fence_sequence"
-    )
     @Column(name = "id")
     private Long id;
 
@@ -35,14 +30,8 @@ public class Fence {
     @Column(name = "description")
     private String description;
 
-    @Column(name = "center_longitude")
-    private Double centerLongitude;
-
-    @Column(name = "center_latitude")
-    private Double centerLatitude;
-
-    @Column(name = "center_radius")
-    private Double centerRadius;
+    @Column(name = "boundary", columnDefinition = "geometry")
+    private Polygon boundary;
 
     @Column(name = "is_active")
     private Boolean isActive;
@@ -50,99 +39,25 @@ public class Fence {
     @OneToMany(mappedBy = "fence", cascade = CascadeType.ALL)
     private List<EventLog> logs;
 
-    //no args constructor
-    public Fence() {
+
+    //added no arg constructor implicitly to overcome a runtime error that requires it
+    public Fence(){
+
     }
 
-    //args constructor
-    public Fence(String name, String description, Double centerLongitude, Double centerLatitude, Double centerRadius, Boolean isActive) {
+    //args constructor with JSONCreator
+    @JsonCreator
+    public Fence(@JsonProperty("id") Long id,
+                 @JsonProperty("name") String name,
+                 @JsonProperty("description") String description,
+                 @JsonProperty("boundary") Polygon boundary,
+                 @JsonProperty("is_active") Boolean isActive) {
+        this.id = id;
         this.name = name;
         this.description = description;
-        this.centerLongitude = centerLongitude;
-        this.centerLatitude = centerLatitude;
-        this.centerRadius = centerRadius;
+        this.boundary = boundary;
         this.isActive = isActive;
     }
 
-    //getters and setters
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Double getCenterLongitude() {
-        return centerLongitude;
-    }
-
-    public void setCenterLongitude(Double centerLongitude) {
-        this.centerLongitude = centerLongitude;
-    }
-
-    public Double getCenterLatitude() {
-        return centerLatitude;
-    }
-
-    public void setCenterLatitude(Double centerLatitude) {
-        this.centerLatitude = centerLatitude;
-    }
-
-    public Double getCenterRadius() {
-        return centerRadius;
-    }
-
-    public void setCenterRadius(Double centerRadius) {
-        this.centerRadius = centerRadius;
-    }
-
-    public Boolean getActive() {
-        return isActive;
-    }
-
-    public void setActive(Boolean active) {
-        isActive = active;
-    }
-
-    public List<EventLog> getLogs() {
-        return logs;
-    }
-
-    public void setLogs(List<EventLog> logs) {
-        this.logs = logs;
-    }
-
-    //to string
-    @Override
-    public String toString() {
-        return "Fence{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                ", centerLongitude=" + centerLongitude +
-                ", centerLatitude=" + centerLatitude +
-                ", centerRadius=" + centerRadius +
-                ", isActive=" + isActive +
-                ", logs=" + logs +
-                '}';
-    }
-
+    //getters/ setters, and toString are handled by Lombok
 }
